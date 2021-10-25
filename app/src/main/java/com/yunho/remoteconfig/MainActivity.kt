3013a071c8c293d418fba8803ac385f7759a7689
DyNamiC <imsong1997@gmail.com>
2021-10-25 20:56:20 +0900

    realtime test

--- app/src/main/java/com/yunho/remoteconfig/MainActivity.kt
+++ app/src/main/java/com/yunho/remoteconfig/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.yunho.remoteconfig
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
+    private val firebaseDatabase = FirebaseDatabase.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val databaseReference = firebaseDatabase.reference
+
+        databaseReference.child("message").push().setValue("2");
     }
 }
